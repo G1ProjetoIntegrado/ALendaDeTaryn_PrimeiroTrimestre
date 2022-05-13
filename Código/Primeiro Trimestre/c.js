8b6ac9480342265de4c931179f90a73430658786
JoaoGabrielMata <joaogabrielmta@gmail>
2022-05-13 09:28:38 -0300

    Terminei totalmente

--- Código/Primeiro Trimestre/c.js
+++ Código/Primeiro Trimestre/c.js
@@ -1,6 +1,6 @@
 //Variavel 
-var x=0 ,y=842, 
-b=0, o=60,
+var x=200,y=841, //parametros da Taryn
+b=0, o=60, //parametros do boneco 
 canvas = document.querySelector('canvas'), //declara que o elemento "canvas" deve ser selecionado
 c = canvas.getContext('2d'), ctx = canvas.getContext('2d')//Desenha o canvas na tela
 canvas.height = window.innerHeight -35 
@@ -9,14 +9,15 @@ canvas.width = window.innerWidth - 35  //determina a altura e largura
 //Iniciando o Jogo 
 alert("Isto é uma representação do Tutorial do jogo 'A lenda de Taryn'")
 alert("Seu objetivo é atingir o boneco 3 vezes") //introdução da apresentação
-alert("Inicie com 'D'") //Instruções de como se iniciar 
+alert("Controles : 'w/a/s/d e o botão esquerdo do mouse'")
+alert("Inicie com 'clique na tela e pressione 'D'") //Instruções de como se iniciar 
 
 
 //Boneco 
 function boneco(){
-    requestAnimationFrame(boneco)
-    c.arc (1600,820,o,0, Math.PI*2,false)
-    c.strokeStyle = 'Blue'
+    requestAnimationFrame(boneco)//a animação do boneco começa
+    c.arc (1600,820,o,0, Math.PI*2,false)//parametros do boneco
+    c.strokeStyle = 'Blue'//Cor do elemento
     c.stroke()} //Desenho do boneco 
 //Taryn 
 function Taryn(){
@@ -30,18 +31,33 @@ function Taryn(){
 if(x  > 1491){ x= 1300
 b=b+1} //Se o elemento for maior que 1491 é adicionado 1 a variavel b
 if(b==3){
-o=0
-alert("Retorne para o acampamento")
-b=0
+o=0//apos a variavel B ser igual a 3 o boneco desaparece. (como se o boneco se quebrasse)
+alert("Retorne para o acampamento") //Instruções ao usuario
+b=0 //Caso não retornasse ele entrava em loop 
 if(x<1490){
-alert("PARA O ACAMPAMENTO!")}
+alert("PARA O ACAMPAMENTO!")} //instruções 
 } //Caso b for igual a 3 o jogo alerta que o elemento deve voltar para a posição inicial
-if(x < 0){ x=1
+if(x < 170){ x=191
 alert("OBRIGADO POR JOGAR NOSSA PRIMEIRA VERSÃO!")}
 if(y > 842){y= 841}
 if(y < 752){ y=841}
 }// colisão do elemento com o cenário canvas
 
+//Reiniciar jogo
+function Reiniciar(){
+    x=200
+    y=841
+    alert("O jogo foi reiniciado!")//Informação para o úsuario 
+}
+//try/Catch
+try{
+if(y>=751){
+Taryn()
+}
+}
+catch{
+console.log("Aperte alguma tecla para forçar o inicio")
+}
 //Movimentação das teclas
 //Eixo X
 document.addEventListener("keydown", function(e){
@@ -74,4 +90,9 @@ document.addEventListener("keydown", function(e){
     y= y +30
     }
 })//Adicionando a movimentação do elemento ao apertar a tecla "s"
-
+//Reinicar o game
+document.addEventListener("keydown", function(e){
+if (e.key == "r") { //confere se a tecla correta foi selecionada
+  Reiniciar()
+    }
+})
